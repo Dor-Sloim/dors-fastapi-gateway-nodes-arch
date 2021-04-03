7cfb6d7a4d3c873666b1934df93f4b6b985f0b59
Dor Sloim <dorsloim@gmail.com>
2021-04-04 00:28:11 +0300

    Removed async with AsyncClient blocks

--- gateway/main.py
+++ gateway/main.py
@@ -1,13 +1,14 @@
 import asyncio
 
+import httpx
 from fastapi import FastAPI
 from httpx import AsyncClient
 from starlette import status
-from starlette.requests import Request
 from starlette.responses import JSONResponse
 
 from common.configuration.constants import ENDPOINT_POST_RESOURCE, ENDPOINT_GET_RESOURCE, ENDPOINT_GATEWAY_POST_NODE, \
     ENDPOINT_GATEWAY_GET_NODE, ENDPOINT_GET_RESOURCE_WITHOUT_ID
+from common.model.node import Node
 from common.model.resource import Resource
 from gateway.core.gateway_state import GatewayState
 
@@ -23,38 +24,39 @@ async def broadcast_post_resource(resource: Resource):
     :param resource: Resource object containing the 'message' data.
     :return: 200 (HTTP_OK) if successful.
     """
-    async with AsyncClient(app=app) as client:
-        all_results = await asyncio.gather(
-            *[post_async(f'{domain}{ENDPOINT_POST_RESOURCE}', client, domain, resource)
-              for domain in state.nodes])
-
+    client = httpx.AsyncClient()
+    all_results = await asyncio.gather(
+        *[post_async(f'http://{domain}{ENDPOINT_POST_RESOURCE}', client, domain, resource) for domain in
+          state.nodes])
+    await client.aclose()
     return JSONResponse(status_code=status.HTTP_200_OK, content={'responses': all_results})
 
 
 @app.get(ENDPOINT_GET_RESOURCE)
-async def broadcast_get_resource(id: int):
+async def broadcast_get_resource(id: str):
     """
     Broadcasts 'get resource' to obtain the message with the given id
     from every known node.
     :param id: The Message id to query
     :return: 200 (HTTP_OK) if successful.
     """
-    async with AsyncClient(app=app) as client:
-        all_results = await asyncio.gather(
-            *[get_async(f'{domain}{ENDPOINT_GET_RESOURCE_WITHOUT_ID}', domain, client, params={'id': id})
-              for domain in state.nodes])
+    client = httpx.AsyncClient()
+    all_results = await asyncio.gather(
+        *[get_async(f'http://{domain}{ENDPOINT_GET_RESOURCE_WITHOUT_ID}/{id}', domain, client)
+          for domain in state.nodes])
+    await client.aclose()
 
     return JSONResponse(status_code=status.HTTP_200_OK, content={'responses': all_results})
 
 
 @app.post(ENDPOINT_GATEWAY_POST_NODE)
-def gateway_post_node(request: Request):
+def gateway_post_node(node: Node):
     """
     Receives nodes registration to the local state.
-    :param request: Request object containing the host and the ip of the client.
+    :param node: Node object containing the host and the ip of the client.
     :return: 201 (HTTP_201_CREATED) if successful.
     """
-    client_domain = f'{request.client.host}:{request.client.port}'
+    client_domain = f'{node.hostname}:{node.port}'
     print(f'New Node registration: {client_domain}')
     is_added = state.add_node(client_domain)
     if is_added:
@@ -72,11 +74,16 @@ def gateway_get_nodes():
     return state.nodes
 
 
-async def get_async(url: str, domain: str, client: AsyncClient, params: dict):
-    res = await client.get(url, params=params)
+async def get_async(url: str, domain: str, client: AsyncClient):
+    res = await client.get(url)
     return {domain: res.json()}
 
 
 async def post_async(url: str, client: AsyncClient, domain: str, resource: Resource):
-    res = await client.post(url, data=resource.dict())
+    print(f'post_async url: {url}')
+    resource_dict = resource.dict()
+    print(f'post_async data: {resource_dict}')
+    res = await client.post(url, json=resource_dict)
+    print(f'post_async res: {res}')
+    print(f'post_async res.json: {res.json()}')
     return {domain: res.json()}
