33c1c97f9c765b36f86d861bba06429e9999e7cb
Dor Sloim <dorsloim@gmail.com>
2021-04-03 13:43:42 +0300

    Added constants.py with common endpoints

---
+++ common/configuration/constants.py
@@ -0,0 +1,6 @@
+ENDPOINT_POST_RESOURCE = "/api/resource"
+ENDPOINT_GET_RESOURCE = "/api/resource/{id}"
+ENDPOINT_GET_RESOURCE_WITHOUT_ID = "/api/resource"
+
+ENDPOINT_GATEWAY_POST_NODE = "/gateway/node"
+ENDPOINT_GATEWAY_GET_NODE = "/gateway/node"
